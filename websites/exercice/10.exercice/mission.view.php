f16ef2be3ceb9c410acd989cd9f0abbd812892b6
jonathan <jonathanbajoux.pro@gmail.com>
2023-03-14 16:04:58 +0100

    laracasts-php

--- websites/exercice/10.exercice/mission.view.php
+++ websites/exercice/10.exercice/mission.view.php
@@ -140,12 +140,11 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home page</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Mission</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <p>Welcome Home</p>
             </div>
         </main>
     </div>
