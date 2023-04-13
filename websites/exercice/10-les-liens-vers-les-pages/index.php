2d79947caf2a2d1e379ae226552b561016ca4776
jonathan <jonathanbajoux.pro@gmail.com>
2023-04-13 14:33:52 +0200

    laracasts-php

--- websites/exercice/10-les-liens-vers-les-pages/index.php
+++ websites/exercice/10-les-liens-vers-les-pages/index.php
@@ -11,8 +11,8 @@ $db = new Database($config['database']);
 
 $id = $_GET['id'];
 
-$query = "SELECT * FROM posts WHERE id = ?";
+$query = "SELECT * FROM posts WHERE id = :id";
 
-$posts = $db->query($query, [$id])->fetch();
+$posts = $db->query($query, ['id' => $id])->fetch();
 
 dd ($posts);
